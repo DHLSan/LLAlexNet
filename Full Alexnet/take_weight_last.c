932df85ea4421f64863224c9a4b5ac0f11b90455
Mehmet Selim Nazlı <64321774+sesleglinm98@users.noreply.github.com>
2021-07-05 23:47:15 +0300

    Update take_weight_last.c

--- Full Alexnet/take_weight_last.c
+++ Full Alexnet/take_weight_last.c
@@ -59,76 +59,7 @@ void take_weight_convolution(int channel1, int channel2, int size,
 
 int main(void)
 {
-	/*
-	int channel1 = 96, channel2 = 3, size = 11;
-	float nums[channel1][channel2][size][size];
-	memset(nums, 0, channel1 * channel2 * size * size * sizeof(float));
-	float kontrol[1] = {0};
-    int i = 0;
-    int j = 0;
-    int k = 0;  
-    int l = 0;
-    int count = 0;
-    int temp = 0;
-    int temp2 = 0;
-    FILE * fp;
- 	char ch;
- 	int sayac = 0;
- 	
- 	char f_open[23] = "weight_0_yuvarlama.txt";
- 	
-    if(fp = fopen(f_open, "r")) {
-        while ((ch = fgetc(fp)) != EOF ) {
-        //printf("%f\n", ch);	
-		fscanf(fp, "%f", &kontrol[0]) != EOF;     // kontrol e atama yapýldý
-        	//printf("%d \n",ch);
-		if(kontrol[0] != 0){                      // kontrol deðeri 0 dan farklý ise
-            nums[i][j][k][l] = kontrol[0];
-			++count;                // nums deðerine aktarýlýr sayý
-			++i;
-			if(i == channel1){
-				i = 0;
-				j = j +1 ;
-				if(j == channel2){
-					j = 0;
-					k = k+1;
-					if(k == size){
-						k = 0;
-						l = l+1;
-					}
-				}
-			}
-            kontrol[0] = 0;
-        }
-        else            // txt dosyasýnda 0 sayýsý olduðunda hata olmamasý için
-        {
-        	if(ch == 32){            			// sayý 0 ise ve "[" karakterine eþit deðilse
-        		nums[i][j][k][l] = kontrol[0];	// içeri girer ve 0 sayýsýný atama yapar
-        		++count;                // nums deðerine aktarýlýr sayý
-			++i;
-			if(i == 96){
-				i = 0;
-				j = j +1 ;
-				if(j == 3){
-					j = 0;
-					k = k+1;
-					if(k == 11){
-						k = 0;
-						l = l+1;
-					}
-				}
-			}
-            kontrol[0] = 0;
-			}		
-		}
-}
-        fclose(fp);
-    }
-    */
-    
-	//temp = j;
-	//temp2 = k;
-	//printf("k = %d && j = %d  &&  count = %d\n", k,j,count);
+
 	int l = 0, k = 0, j = 0, i = 0, sayac = 0;
 	
 	int channel1 = 256, channel2 = 48, size = 5;
