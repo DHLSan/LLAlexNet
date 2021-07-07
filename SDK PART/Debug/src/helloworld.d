7175a80044d6cea15248f66ca136b9f94cf8cc0c
Arif Berkay Okumus <57463112+arifoku@users.noreply.github.com>
2021-07-07 16:35:30 +0300

    Add files via upload

---
+++ SDK PART/Debug/src/helloworld.d
@@ -0,0 +1,66 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xconv_upg2.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_assert.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xstatus.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xconv_upg2_hw.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xconv_upg2_hw.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xtime_l.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xtmrctr.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xtmrctr_l.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xconv_upg2.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_assert.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xstatus.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xconv_upg2_hw.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xconv_upg2_hw.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xtime_l.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xtmrctr.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xtmrctr_l.h:
---
+++ SDK PART/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../llalexnet_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../llalexnet_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
